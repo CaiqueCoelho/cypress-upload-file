7c073b2be4190c7117f4ab330bbfa980a2e3d13a
Caique Coelho <caique.coelho@quintoandar.com.br>
2020-11-02 23:21:06 -0300

    update uploadFile

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-function uploadFile(subject, fileName, fileType = '') {
-  cy.fixture(fileName,'base64')
-  .then( (content) => Cypress.Blob.base64StringToBlob(content, fileType))
-  .then( (blob) => {
-      const el = subject[0];
-      const testFile = new File([blob], fileName, {type: fileType});
+import hexStringToByte from "./utils";
+
+function uploadFile(selectorElement, fileName, fileType) {
+  cy.get(selectorElement).then((subject) => {
+    cy.fixture(fileName, "hex").then((fileHex) => {
+      const fileBytes = hexStringToByte(fileHex);
+      const testFile = new File([fileBytes], fileName, {
+        type: fileType,
+      });
       const dataTransfer = new DataTransfer();
+      const el = subject[0];
 
       dataTransfer.items.add(testFile);
       el.files = dataTransfer.files;
-      cy.wrap(subject).trigger('change', { force: true });
     });
+  });
 }
 
-module.exports = uploadFile
+module.exports = uploadFile;
