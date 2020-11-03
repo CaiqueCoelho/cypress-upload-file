6d6de054075cf3eaee46c56c843ebf2514e240ad
Caique Coelho <caique.coelho@quintoandar.com.br>
2020-11-03 00:11:34 -0300

    insert documentation

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import hexStringToByte from "./utils";
 
+/**
+ * This function help cypress to upload a file to a file upload input
+ * using any type of file extension: png, jpg, jpeg, gif, pdf, svg, mp3, docx.
+ * You need to have your file stored in fixtures cypress folder
+ * Call this function with some selector element, with file name, and file type
+ *
+ * @param  {String} selectorElement Some selector information to find the file upload input element
+ * @param  {String} fileName Name of the file with with its extension, exampĺe image.png
+ * @param {String}      fileType The file type extension like: png, jpeg, jpg, pdf
+ * @return {VoidFunction}      This function don't returned anything
+ */
+
 function uploadFile(selectorElement, fileName, fileType) {
   cy.get(selectorElement).then((subject) => {
     cy.fixture(fileName, "hex").then((fileHex) => {
@@ -12,6 +24,12 @@ function uploadFile(selectorElement, fileName, fileType) {
 
       dataTransfer.items.add(testFile);
       el.files = dataTransfer.files;
+
+      Cypress.log({
+        name: "uploadFile",
+        displayName: "FILE",
+        message: fileName,
+      });
     });
   });
 }
